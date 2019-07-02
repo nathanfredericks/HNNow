a19bb0d976f3affd0069a54a8668b62b3eabc9e2
Nathaniel Fredericks <me@nathfreder.dev>
2019-07-02 09:50:38 -0300

    Rename to HN Now

---
+++ HNNow/ContentView.swift
@@ -0,0 +1,60 @@
+//
+//  ContentView.swift
+//  HNNow
+//
+//  Created by Nathaniel Fredericks on 2019-06-09.
+//  Copyright © 2019 Nathaniel Fredericks. All rights reserved.
+//
+
+import SwiftUI
+
+enum FeedType: String, CaseIterable {
+    case top = "Top"
+    case new = "New"
+    case best = "Best"
+}
+
+struct ContentView : View {
+    @ObjectBinding var store = StoryStore()
+    
+    var body: some View {
+        NavigationView {
+            List {
+                SegmentedControl(selection: $store.feedType) {
+                    ForEach(FeedType.allCases.identified(by: \.self)) { type in
+                        Text(type.rawValue).tag(type)
+                    }
+                }
+                
+                ForEach(store.stories) { story in
+                    NavigationButton(destination: StoryWebView(story: story)) {
+                        StoryRow(story: story)
+                    }
+                }
+                }
+                .navigationBarTitle(Text("HN Now"))
+                .navigationBarItems(trailing: Button(action: {
+                    guard !self.store.isLoading else { return }
+                    
+                    self.store.fetchStories(feed: self.$store.feedType.value)
+                }) {
+                    if store.isLoading {
+                        ActivityIndicatorView(style: .medium)
+                    } else {
+                        Image(systemName: "arrow.clockwise")
+                            .foregroundColor(.blue)
+                            .accessibility(label: Text("Reload"))
+                    }
+                    }
+            )
+        }
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
---
+++ HNNow/Models/StoryStore.swift
@@ -0,0 +1,55 @@
+//
+//  StoryStore.swift
+//  HNNow
+//
+//  Created by Nathaniel Fredericks on 2019-06-09.
+//  Copyright © 2019 Nathaniel Fredericks. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+import Combine
+
+final class StoryStore: BindableObject {
+    var didChange = PassthroughSubject<StoryStore, Never>()
+    
+    var feedType: FeedType = .top {
+        didSet {
+            stories.removeAll()
+            fetchStories(feed: feedType)
+        }
+    }
+    
+    var stories = [Story]() {
+        didSet { didChange.send(self) }
+    }
+    
+    private(set) var isLoading: Bool = false {
+        didSet { didChange.send(self) }
+    }
+    
+    func fetchStories(feed: FeedType) {
+        let hackerNewsService = HackerNewsService()
+        
+        isLoading = true
+        
+        hackerNewsService.fetchStories(feed: feed) { (stories, error) in
+            guard error == nil else {
+                self.isLoading = false
+                return
+            }
+            
+            guard let stories = stories else {
+                self.isLoading = false
+                return
+            }
+            
+            self.stories = stories
+            self.isLoading = false
+        }
+    }
+    
+    init() {
+        fetchStories(feed: feedType)
+    }
+}
